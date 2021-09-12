8c6401a1d9b1fd3d9f4a7092dc614e934daac1bf
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-11 20:40:15 -0500

    Programming to an interface

--- 01-Programming-To-Interface/src/main/java/programtointerface/Computer.java
+++ 01-Programming-To-Interface/src/main/java/programtointerface/Computer.java
@@ -1,8 +1,41 @@
 package programtointerface;
 
-public class Computer {
-    public static void main(String[] args) {
+interface DisplayModule {
+    public void display();
+}
 
+class Monitor implements DisplayModule {
+    public void display() {
+        System.out.println("Display through Monitor");
+    }
+}
+
+class Projector implements DisplayModule {
+    public void display() {
+        System.out.println("Display through Projector");
+    }
+}
+
+public class Computer {
+    DisplayModule dm;
+
+    public void setDisplayModule(DisplayModule dm) {
+        this.dm = dm;
+    }
+
+    public void display() {
+        dm.display();
+    }
+
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        DisplayModule dm = new Monitor();
+        DisplayModule dm1 = new Projector();
+
+        computer.setDisplayModule(dm);
+        computer.display();
+        computer.setDisplayModule(dm1);
+        computer.display();
     }
 
 }
