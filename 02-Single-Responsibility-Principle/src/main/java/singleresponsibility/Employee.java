ef57e5d79b52a28e1990e7c2bc1af43c5b0d5203
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-12 12:39:30 -0500

    Single responsibility principle

---
+++ 02-Single-Responsibility-Principle/src/main/java/singleresponsibility/Employee.java
@@ -0,0 +1,20 @@
+package singleresponsibility;
+
+import java.util.Date;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+    private String address;
+    private Date dateOfJoining;
+
+    public boolean isPromotionDueThisYear() {
+        //promotion logic
+    }
+
+    public double calcIncomeTaxForCurrentYear() {
+
+    }
+
+    // getters // setters
+}
