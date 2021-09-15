f282f7fcf00c2c440dde1273b968b95d6fc8a1f7
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-14 23:47:41 -0500

    Open Closed Principle

--- 03-Open-Closed-Principle/src/main/java/openclosedprinciple/OpenClosedExample.java
+++ 03-Open-Closed-Principle/src/main/java/openclosedprinciple/OpenClosedExample.java
@@ -1,26 +1,45 @@
 package openclosedprinciple;
 
-class Rectangle {
+interface Shape {
+    public double calculateArea();
+}
+
+class Rectangle implements Shape {
     public double length;
     public double width;
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Circle implements Shape {
+    public double radius;
+
+    public double calculateArea() {
+        return (22/7) * radius * radius;
+    }
 }
 
 class AreaCalculator {
-    public double calculateRectangleArea(Rectangle rectangle){
-        return rectangle.length * rectangle.width;
+    public double calculateShapeArea(Shape shape) {
+        return shape.calculateArea();
     }
-
-    public double calculateCirleArea(Circle circle) {
-        return (22/7) * circle.radius * circle.radius;
-    }
-}
-
-class Circle {
-    public double radius;
 }
 
 public class OpenClosedExample {
     public static void main(String[] args) {
+        AreaCalculator areaCalculator = new AreaCalculator();
+
+        Circle circle = new Circle();
+        circle.radius = 10;
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 5;
+        rectangle.length = 10;
+
+        System.out.println(areaCalculator.calculateShapeArea(circle));
+        System.out.println(areaCalculator.calculateShapeArea(rectangle));
 
     }
 }
