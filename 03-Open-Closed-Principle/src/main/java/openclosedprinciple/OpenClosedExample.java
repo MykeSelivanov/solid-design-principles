d24ae4367006cf4ed81dd4f16b0e5003429fe238
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-14 23:40:32 -0500

    Open Closed Principle

--- 03-Open-Closed-Principle/src/main/java/openclosedprinciple/OpenClosedExample.java
+++ 03-Open-Closed-Principle/src/main/java/openclosedprinciple/OpenClosedExample.java
@@ -1,5 +1,24 @@
 package openclosedprinciple;
 
+class Rectangle {
+    public double length;
+    public double width;
+}
+
+class AreaCalculator {
+    public double calculateRectangleArea(Rectangle rectangle){
+        return rectangle.length * rectangle.width;
+    }
+
+    public double calculateCirleArea(Circle circle) {
+        return (22/7) * circle.radius * circle.radius;
+    }
+}
+
+class Circle {
+    public double radius;
+}
+
 public class OpenClosedExample {
     public static void main(String[] args) {
 
