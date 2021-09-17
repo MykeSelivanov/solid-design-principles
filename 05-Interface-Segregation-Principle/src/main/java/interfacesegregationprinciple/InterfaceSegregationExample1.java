cbb20f80871243dbb1e5f5809e6f82e41d435b50
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-16 21:51:16 -0500

    Interface Segregation Principle

---
+++ 05-Interface-Segregation-Principle/src/main/java/interfacesegregationprinciple/InterfaceSegregationExample1.java
@@ -0,0 +1,51 @@
+package interfacesegregationprinciple;
+
+interface ShapeInterface {
+    public double area();
+}
+
+interface SolidShapeInterface {
+    public double volume();
+}
+
+interface ManageShapeInterface {
+    public double calculate();
+}
+
+class Cube implements ShapeInterface, SolidShapeInterface, ManageShapeInterface {
+    @Override
+    public double area() {
+        // calculate the surface area of the cuboid
+        return 1.0;
+    }
+
+    @Override
+    public double volume() {
+        // calculate the volume of the cuboid
+        return 1.0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area() + this.volume();
+    }
+}
+
+class Square implements ShapeInterface, ManageShapeInterface {
+    @Override
+    public double area() {
+        return 2.0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area();
+    }
+}
+
+
+public class InterfaceSegregationExample1 {
+    public static void main(String[] args) {
+
+    }
+}
