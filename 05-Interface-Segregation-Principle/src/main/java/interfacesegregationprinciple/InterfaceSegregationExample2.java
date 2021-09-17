90a9252e1789f6c22854a482fcd4986f2675e1fb
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-16 22:25:28 -0500

    Interface Segregation Principle

---
+++ 05-Interface-Segregation-Principle/src/main/java/interfacesegregationprinciple/InterfaceSegregationExample2.java
@@ -0,0 +1,15 @@
+package interfacesegregationprinciple;
+
+interface RestaurantInterface {
+    public void acceptOnlineOrder();
+    public void takeTelephoneOrder();
+    public void payOnline();
+    public void walkInCustomerOrder();
+    public void payInPerson();
+}
+
+public class InterfaceSegregationExample2 {
+    public static void main(String[] args) {
+
+    }
+}
