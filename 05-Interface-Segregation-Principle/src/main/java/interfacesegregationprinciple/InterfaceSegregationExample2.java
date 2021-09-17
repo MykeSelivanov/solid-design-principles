e0fd3499fcbc64f6cf410786eabc76db5ef9cf0c
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-16 22:42:04 -0500

    Interface Segregation Principle

--- 05-Interface-Segregation-Principle/src/main/java/interfacesegregationprinciple/InterfaceSegregationExample2.java
+++ 05-Interface-Segregation-Principle/src/main/java/interfacesegregationprinciple/InterfaceSegregationExample2.java
@@ -1,11 +1,33 @@
 package interfacesegregationprinciple;
 
 interface RestaurantInterface {
-    public void acceptOnlineOrder();
-    public void takeTelephoneOrder();
-    public void payOnline();
     public void walkInCustomerOrder();
+//    public void payInPerson();
+//    public void payOnline();
+//    public void acceptOnlineOrder();
+//    public void takeTelephoneOrder();
+}
+
+interface TelephoneOrder {
     public void payInPerson();
+    public void takeTelephoneOrder();
+}
+
+interface OnlineOrder {
+    public void payOnline();
+    public void acceptOnlineOrder();
+}
+
+class OnlineClientImpl implements OnlineOrder {
+    @Override
+    public void payOnline() {
+
+    }
+
+    @Override
+    public void acceptOnlineOrder() {
+
+    }
 }
 
 public class InterfaceSegregationExample2 {
