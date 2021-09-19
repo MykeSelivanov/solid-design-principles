e85ff86b872f6b36eeb7177a20773da943891c50
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-19 15:17:02 -0500

    Dependency Inversion Principle

--- 06-Dependency-Inversion-Principle/src/main/java/dependencyinversionexample/DependencyInversionExample.java
+++ 06-Dependency-Inversion-Principle/src/main/java/dependencyinversionexample/DependencyInversionExample.java
@@ -1,5 +1,24 @@
 package dependencyinversionexample;
 
+interface DBConnectionInterface {
+    public int connect();
+}
+
+class MySqlConnection implements DBConnectionInterface {
+    public int connect() {
+        // connection logic
+        return 1;
+    }
+}
+
+class PasswordReminder {
+    private DBConnectionInterface dbConnection;
+
+    public PasswordReminder(DBConnectionInterface dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+}
+
 public class DependencyInversionExample {
     public static void main(String[] args) {
 
