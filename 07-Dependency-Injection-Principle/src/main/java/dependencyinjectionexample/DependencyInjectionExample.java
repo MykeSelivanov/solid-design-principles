76f7e2df09a35da2cc025af429b9a4ec29b6ed99
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-21 22:27:36 -0500

    Dependency Injection Principle

--- 07-Dependency-Injection-Principle/src/main/java/dependencyinjectionexample/DependencyInjectionExample.java
+++ 07-Dependency-Injection-Principle/src/main/java/dependencyinjectionexample/DependencyInjectionExample.java
@@ -4,8 +4,31 @@ interface Service {
     void write(String message);
 }
 
+class ServiceA implements Service {
+    @Override
+    public void write(String message){
+        System.out.println(message);
+    }
+}
+
+class Client {
+    private Service myService;
+
+    // injection via constructor
+    public Client(Service myService) {
+        this.myService = myService;
+    }
+
+    public void doSomething() {
+        myService.write("This is a message through constructor injection");
+    }
+}
+
 public class DependencyInjectionExample {
     public static void main(String[] args) {
+        Service service = new ServiceA(); // the injector
+        Client client = new Client(service); // injects via constructor
+        client.doSomething();
 
     }
 }
