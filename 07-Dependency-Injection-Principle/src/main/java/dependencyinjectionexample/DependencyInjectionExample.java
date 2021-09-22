9b0f68ace0ead420e28f4084501a5ad583b5bbaa
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-21 22:20:21 -0500

    Dependency Injection Principle

---
+++ 07-Dependency-Injection-Principle/src/main/java/dependencyinjectionexample/DependencyInjectionExample.java
@@ -0,0 +1,11 @@
+package dependencyinjectionexample;
+
+interface Service {
+    void write(String message);
+}
+
+public class DependencyInjectionExample {
+    public static void main(String[] args) {
+
+    }
+}
